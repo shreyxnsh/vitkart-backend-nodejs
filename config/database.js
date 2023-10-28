829fab35e823ba71f28f6575c0752c65bfdcf411
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-29 00:06:42 +0530

    minor fixes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/vitkart-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000, // Increase the server selection timeout to 30 seconds
+    serverSelectionTimeoutMS: 5000, // Increase the server selection timeout to 30 seconds
     socketTimeoutMS: 30000,  // Increase the server selection timeout to 30 seconds
 });
 
--- routers/product.router.js
+++ routers/product.router.js
@@ -3,10 +3,10 @@ const productController = require('../controller/product.controller');
 
 
 //create todo task
-router.post('/createproduct', productController.createCategory );
+router.post('/createproduct', productController.createProduct );
 
 // get all the todo tasks of the user with userId as query parameter
-router.get('/getproductbycat', productController.getProductByCategory);
+router.get('/getproductbycat', productController.getProductsByCategory);
 
 // delete todo task of a user
 router.delete('/deleteproduct', productController.deleteProduct );
