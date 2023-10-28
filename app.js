9b29ee815ab76f509259bc3b68ad9e07a557b1cd
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-29 00:07:07 +0530

    added routes in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const userRouter = require('./routers/user.router');
 const categoryRouter = require('./routers/category.router');
 const productRouter = require('./routers/product.router');
+const cartRouter = require('./routers/cart.router');
 
 
 // body parser is used to get data which comes in the body 
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use('/', userRouter);
 app.use('/', categoryRouter);
 app.use('/', productRouter);
+app.use('/', cartRouter);
 
 
 module.exports = app;
