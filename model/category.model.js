23564822c3f2bdd1508b4cf52828fac3ab15f10d
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-28 18:43:39 +0530

    created category model + services

---
+++ model/category.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+// this database connectivity will create a model in that particular db
+const db = require('../config/database');
+
+// create user schema
+const{Schema} = mongoose;
+
+// schema for the todo items
+const categorySchema = new Schema({
+
+    categoryName:{
+        type: String,
+        required: true,
+    },
+    categoryDesc:{
+        type: String,
+        required: true,
+    },
+    categoryImage:{
+        type: String,
+    }
+});
+
+// this line will name the collection in the db
+const categoryModal = db.model('category', categorySchema);
+
+module.exports = categoryModal;
