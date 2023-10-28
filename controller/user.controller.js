ef667ff0474c9a57fe2407b447b1add660969d48
shreyxnsh <shreyanshatwork@gmail.com>
2023-10-28 20:25:47 +0530

    deleted user services and combined in controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,47 +1,53 @@
-const UserService = require('../services/user.services');
+const userModel = require('../model/user.model');
+const jwt = require('jsonwebtoken');
 
-// register funtion for register api
-exports.register = async (req, res, next)=>{
+// Function to register a new user
+exports.register = async (req, res, next) => {
     try {
-        const {email,password} = req.body;
-        const successRes = await UserService.registerUser(email,password);
-        res.json({status:true, success: "User registered successfully"}); 
-
+        const { email, password } = req.body;
+        const createUser = new userModel({ email, password });
+        const successRes = await createUser.save();
+        res.json({ status: true, success: 'User registered successfully' });
     } catch (error) {
         throw error;
     }
 }
 
-// login function for login api
-exports.login = async (req, res, next)=>{
+// Function to check if a user is registered in the database
+exports.checkUser = async (email) => {
     try {
-        // take 2 parameter, email and password for logging in
-        const {email,password} = req.body;
+        return await userModel.findOne({ email });
+    } catch (error) {
+        throw error;
+    }
+}
 
-        // check if email is registered in the database
-        const user = await UserService.checkUser(email);
-        if(!user){
+// Function to generate a JWT token for user login
+exports.generateJWT = async (tokenData, secretKey, jwt_expire) => {
+    return jwt.sign(tokenData, secretKey, { expiresIn: jwt_expire });
+}
+
+// Function for user login
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await exports.checkUser(email);
+        
+        if (!user) {
             throw new Error('User does not exist');
         }
 
-        // passing the user entered password to the password in the db
         const isMatch = await user.comparePassword(password);
-        if(isMatch == false){
+
+        if (isMatch === false) {
             throw new Error('Password is not valid');
-        };
+        }
 
+        const tokenData = { _id: user._id, email: user.email };
+        const token = await exports.generateJWT(tokenData, 'secretkey', '1h');
 
-        // create a jwt token if email and pass entered are correct and do operation in frontend
-        let tokenData = {_id:user._id, email:user.email};
-
-        // creating token using the generate function created in services
-        const token = await UserService.generateJWT(tokenData,"secretkey",'1h');
-
-        //now we have to send the token as a response for login
-        res.status(200).json({status:true, token:token});
-
+        res.status(200).json({ status: true, token: token });
     } catch (error) {
         throw error;
     }
 }
-
