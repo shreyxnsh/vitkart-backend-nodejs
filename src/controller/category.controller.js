6083f6c94ae92c4cb37f42e84244c6b79b6bbb44
shreyxnsh <shreyanshatwork@gmail.com>
2023-11-15 14:37:01 +0530

    implemented multer for image upload to AWS S3

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -1,16 +1,45 @@
 const categoryModel = require('../src/../model/category.model');
 
+const aws = require('aws-sdk');
+const fs = require('fs');
+
+
 // Create a new category
-exports.createCategory = async (req, res, next) => {
-    try {
-        const { categoryName, categoryDesc, categoryImage } = req.body;
-        const newCategory = new categoryModel({ categoryName, categoryDesc, categoryImage });
-        const createdCategory = await newCategory.save();
-        res.json({ status: true, success: createdCategory });
-    } catch (error) {
-        next(error);
-    }
-}
+exports.createCategory = async (req, res) => {
+    aws.config.setPromisesDependency();
+    aws.config.update({
+      accessKeyId: 'AKIAZGHOSMTD53H7EO53',
+      secretAccessKey: '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',
+      region: 'ap-south-1'
+    });
+    const s3 = new aws.S3();
+    var params = {
+      ACL: 'public-read',
+      Bucket: 'vitkart',
+      Body: fs.createReadStream(req.file.path),
+      Key: `categories/${req.file.originalname}`
+    };
+  
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log('Error occured while trying to upload to S3 bucket', err);
+      }
+  
+      if (data) {
+        fs.unlinkSync(req.file.path); // Empty temp folder
+        const locationUrl = data.Location;
+        let newCategory = new categoryModel({ ...req.body, categoryImage: locationUrl });
+        newCategory
+          .save()
+          .then(category => {
+            res.json({ message: 'User created successfully', category });
+          })
+          .catch(err => {
+            console.log('Error occured while trying to save to DB');
+          });
+      }
+    });
+  }
 
 // Get all categories
 exports.getCategories = async (req, res, next) => {
