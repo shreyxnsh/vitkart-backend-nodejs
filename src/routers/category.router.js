f4ce4c5477b086c620d2698dc0dacb9b653506f9
shreyxnsh <shreyanshatwork@gmail.com>
2023-11-15 14:37:44 +0530

    updated createCategory route

--- src/routers/category.router.js
+++ src/routers/category.router.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
+const multer = require('multer');
 const categoryController = require('../src/../controller/category.controller');
 
 
 //create category
-router.post('/createCategory', categoryController.createCategory );
+const { createCategory } = categoryController;
 
+
+router
+  .route('/createCategory')
+  .post(
+    multer({ dest: 'temp/', limits: { fieldSize: 8 * 1024 * 1024 } }).single(
+      'categoryImage'
+    ),
+    createCategory
+  );
 // get all categories
 router.get('/getCategory', categoryController.getCategories);
 router.get('/getCategory/:id', categoryController.getCategoryById);
