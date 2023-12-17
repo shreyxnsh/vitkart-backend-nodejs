07d3bbcb889ea31c0403966de1ce69d61052bfd4
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-17 10:41:05 +0530

    kyc + product controller updated for images

--- src/controller/kyc.controller.js
+++ src/controller/kyc.controller.js
@@ -1,23 +1,63 @@
 const kycModel = require('../src/../model/kyc.model');
+const aws = require('aws-sdk');
+const fs = require('fs');
+require('dotenv').config();
 
-// Add KYC details
-exports.createKyc = async (req, res, next) => {
-    try {
-        const { firstName, lastName, regNo, email, contact, joiningYear, source } = req.body;
 
-        // Check if the provided regNo, email, and contact are unique
-        const existingKyc = await kycModel.findOne({ $or: [{ regNo }, { email }, { contact }] });
-        if (existingKyc) {
-            return res.status(400).json({ status: false, error: 'KYC with the same regNo, email, or contact already exists.' });
-        }
+// // Add KYC details
+// exports.createKyc = async (req, res, next) => {
+//     try {
+//         const { firstName, lastName, regNo, email, contact, joiningYear, source } = req.body;
 
-        const addKyc = new kycModel({ firstName, lastName, regNo, email, contact, joiningYear, source });
-        const createKyc = await addKyc.save();
-        res.json({ status: true, success: createKyc });
-    } catch (error) {
-        next(error);
-    }
-}
+//         // Check if the provided regNo, email, and contact are unique
+//         const existingKyc = await kycModel.findOne({ $or: [{ regNo }, { email }, { contact }] });
+//         if (existingKyc) {
+//             return res.status(400).json({ status: false, error: 'KYC with the same regNo, email, or contact already exists.' });
+//         }
+
+//         const addKyc = new kycModel({ firstName, lastName, regNo, email, contact, joiningYear, source });
+//         const createKyc = await addKyc.save();
+//         res.json({ status: true, success: createKyc });
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
+exports.createKyc = async (req, res) => {
+    aws.config.setPromisesDependency();
+    aws.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: process.env.AWS_S3_REGION
+    });
+    const s3 = new aws.S3();
+    var params = {
+      ACL: 'public-read',
+      Bucket: process.env.AWS_S3_BUCKET,
+      Body: fs.createReadStream(req.file.path),
+      Key: `profilePics/${req.file.originalname}`
+    };
+  
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log('Error occured while trying to upload to S3 bucket', err);
+      }
+  
+      if (data) {
+        fs.unlinkSync(req.file.path); // Empty temp folder
+        const locationUrl = data.Location;
+        let newUser = new kycModel({ ...req.body, profilePic: locationUrl });
+        newUser
+          .save()
+          .then(kyc => {
+            res.json({ message: 'KYC submitted successfully', kyc });
+          })
+          .catch(err => {
+            console.log('Error occured while trying to save to DB');
+          });
+      }
+    });
+  }
 
 // Get all KYC details
 exports.getKyc = async (req, res, next) => {
--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,49 +1,84 @@
-const ProductModel = require('../src/../model/product.model');
-const mongoose = require('mongoose');
+const aws = require('aws-sdk');
+const fs = require('fs');
+const productModel = require('../src/../model/product.model');
+require('dotenv').config();
 
-// Function to create a product
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = async (req, res) => {
+    aws.config.setPromisesDependency();
+    aws.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: process.env.AWS_S3_REGION
+    });
+    const s3 = new aws.S3();
+    var params = {
+      ACL: 'public-read',
+      Bucket: process.env.AWS_S3_BUCKET,
+      Body: fs.createReadStream(req.file.path),
+      Key: `products/${req.file.originalname}`
+    };
+  
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log('Error occured while trying to upload to S3 bucket', err);
+      }
+  
+      if (data) {
+        fs.unlinkSync(req.file.path); // Empty temp folder
+        const locationUrl = data.Location;
+        let newProduct = new productModel({ ...req.body, productImage: locationUrl });
+        newProduct
+          .save()
+          .then(product => {
+            res.json({ message: 'Product added successfully', product });
+          })
+          .catch(err => {
+            console.log('Error occured while trying to save to DB');
+          });
+      }
+    });
+  }
+
+
+exports.getProducts = async (req, res, next) => {
     try {
-        // Initializing parameters in the request body
-        const { productName, productDesc, productImage, productCat, productPrice, stock } = req.body;
-        // Create a new product using the ProductModel
-        const createdProduct = new ProductModel({ productName, productDesc, productImage, productCat, productPrice, stock });
-        // Save the product to the database
-        const product = await createdProduct.save();
-        
-        res.json({ status: true, success: product });
+        const products = await productModel.find();
+        res.json({ status: true, success: products });
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+exports.getProductbyId = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const productData = await productModel.findById(id);
+        res.json({ status: true, success: productData });
     } catch (error) {
         next(error);
     }
 }
 
-// Get a list of all products for a specific product category
 exports.getProductsByCategory = async (req, res, next) => {
     try {
-        const { productCat } = req.query;
-        
-        // Check if the productCat is a valid ObjectId
-        if (!mongoose.Types.ObjectId.isValid(productCat)) {
-            return res.status(400).json({ status: false, error: 'Invalid category ID' });
-        }
-
-        // Call the service to get products by category
-        const products = await ProductModel.find({ productCat }).populate('productCat');
+        const { productCat } = req.params;
+        const products = await productModel.find({ productCat });
         res.json({ status: true, success: products });
     } catch (error) {
         next(error);
     }
 };
-
 // Function to delete a product by ID
 exports.deleteProduct = async (req, res, next) => {
     try {
         // Extract the product ID from the query parameters
         const { id } = req.body;
         // Delete the product
-        const deletedProduct = await ProductModel.findOneAndDelete({ _id: id });
+        const deletedProduct = await productModel.findOneAndDelete({ _id: id });
         res.json({ status: true, success: deletedProduct });
     } catch (error) {
         next(error);
     }
 }
+
