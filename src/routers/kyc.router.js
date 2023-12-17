48b13f3b8abadcd31519b92343a24015a4b01f2e
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-17 10:38:46 +0530

    updated routes for multer

--- src/routers/kyc.router.js
+++ src/routers/kyc.router.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
+const multer = require('multer');
 const kycController = require('../src/../controller/kyc.controller');
 
-
+const { createKyc } = kycController;
+router
+  .route('/addKyc')
+  .post(
+    multer({ dest: 'temp/', limits: { fieldSize: 8 * 1024 * 1024 } }).single(
+      'profilePic'
+    ),
+    createKyc
+  );
 //add kyc details for user
-router.post('/addKyc', kycController.createKyc );
+router
 
 // get kyc of all users
 router.get('/getKyc', kycController.getKyc);
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
+const multer = require('multer');
 const productController = require('../src/../controller/product.controller');
 
+// create a product
+const { createProduct } = productController;
+router
+  .route('/createProduct')
+  .post(
+    multer({ dest: 'temp/', limits: { fieldSize: 8 * 1024 * 1024 } }).single(
+      'productImage'
+    ),
+    createProduct
+  );
 
-//create todo task
-router.post('/createProduct', productController.createProduct );
-
-// get all the todo tasks of the user with userId as query parameter
-router.get('/getproductbycat', productController.getProductsByCategory);
+  // get all products
+router.get('/getProducts', productController.getProducts);
+// get products by their id 
+router.get('/getProductbyId/:id', productController.getProductbyId);
+// get products by their category
+router.get('/getProductbyCat/:productCat', productController.getProductsByCategory);
 
 // delete todo task of a user
 router.delete('/deleteproduct', productController.deleteProduct );
