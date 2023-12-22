f0f2b6626328d9b802d8b848e4c9ce3db81179fc
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-22 14:31:49 +0530

    changed db connection setup

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 
 // to get everything which is mentioned in the app folder.
 const app = require('./app');
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 //create a port for the server
 const port = 3001;
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 // get request for route
 app.get('/', (req, res)=>{
