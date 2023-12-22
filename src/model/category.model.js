da028d827ce4bd4663b9d67631ed9d3a704a839e
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-22 14:36:18 +0530

    kyc + category model updated

--- src/model/category.model.js
+++ src/model/category.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // this database connectivity will create a model in that particular db
-const db = require('../src/../config/database');
+
 
 // create user schema
 const{Schema} = mongoose;
@@ -22,6 +22,6 @@ const categorySchema = new Schema({
 });
 
 // this line will name the collection in the db
-const categoryModal = db.model('category', categorySchema);
+const categoryModal = mongoose.model('category', categorySchema);
 
 module.exports = categoryModal;
--- src/model/kyc.model.js
+++ src/model/kyc.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // this database connectivity will create a model in that particular db
-const db = require('../src/../config/database');
+
 
 // create user schema
 const{Schema} = mongoose;
@@ -50,6 +50,6 @@ const kycSchema = new Schema({
 );
 
 // this line will name the collection in the db
-const kycModel = db.model('kyc', kycSchema);
+const kycModel = mongoose.model('kyc', kycSchema);
 
 module.exports = kycModel;
