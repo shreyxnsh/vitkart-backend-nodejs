ea296396dd9d8505fff8f14fd370622eaa11c2d5
shreyxnsh <shreyanshatwork@gmail.com>
2023-12-25 16:48:54 +0530

    cart completed

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 // file to use express to create routes for the apis
 const express = require('express');
 const app = express();
-
 require('dotenv').config();
-
 const bodyParser = require('body-parser');
 const userRouter = require('./src/routers/user.router');
 const categoryRouter = require('./src/routers/category.router');
 const productRouter = require('./src/routers/product.router');
 const cartRouter = require('./src/routers/cart.router');
 const kycRouter = require('./src/routers/kyc.router');
-
-
-
+const authRouter = require('./src/routers/auth.router');
+const orderRouter = require('./src/routers/order.router');
 
 // body parser is used to get data which comes in the body 
 app.use(bodyParser.json());
@@ -22,14 +19,11 @@ app.use(bodyParser.json());
 // this function will use the services of user registration
 // app.use('/api/v1', userRouter);
 app.use('/api/v1', categoryRouter);
-app.use('/api/v1', productRouter);
-app.use('/api/v1', cartRouter);
+app.use('/api/v1/product', productRouter);
+app.use('/api/v1/cart', cartRouter);
 app.use('/api/v1', kycRouter);
-app.use('/api/v1', userRouter);
-
-
-
-  
-
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/order', orderRouter);
 
 module.exports = app;
--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -1,9 +1,20 @@
-const router = require('express').Router();
-const cartController = require('../src/../controller/cart.controller');
+const router = require("express").Router();
+const cartController = require("../controller/cart.controller");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("../middleware/verifyToken");
 
-router.post('/addCart', cartController.addToCart);
-router.get('/getCart', cartController.viewCart);
-router.put('/updateCartProduct', cartController.updateCart);
-router.delete('/removeFromCart', cartController.deleteFromCart);
+// CREATE
+router.post("/", verifyToken, cartController.createCart);
+
+// UPDATE
+router.put("/:id", verifyTokenAndAuthorization, cartController.updateCart);
+
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, cartController.deleteCartItem);
+
+// GET USER CART
+router.get("/find/:userId", verifyTokenAndAuthorization, cartController.findUserCart);
+
+// GET ALL CARTS
+router.get("/", verifyTokenAndAdmin, cartController.getAllCarts);
 
 module.exports = router;
